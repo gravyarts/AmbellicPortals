04a4eb27b08b52f31914507b3edd298f01fd99b4
InitStudios <43098489+Asc4Yin@users.noreply.github.com>
2020-03-25 00:31:29 -0700

    Fixes and new stuff
    Added portal Panel Blocks and Portable Surfaces

--- src/main/java/com/ambellic/ambellicportals/AmbellicPortals.java
+++ src/main/java/com/ambellic/ambellicportals/AmbellicPortals.java
@@ -1,8 +1,6 @@
 package com.ambellic.ambellicportals;
 
-import net.minecraft.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -12,15 +10,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 @Mod("ambellicportals")
 public class AmbellicPortals
 {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "ambellicportals";
     public static AmbellicPortals instance;
+
     public AmbellicPortals()
+
     {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
@@ -30,17 +28,14 @@ public class AmbellicPortals
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) { }
 
-    }
-    private void doClientStuff(final FMLClientSetupEvent event)
-    {
 
-    }
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+
+
     @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event)
-    {
+    public void onServerStarting(FMLServerStartingEvent event) { }
 
-    }
+
 }
--- src/main/java/com/ambellic/ambellicportals/init/ItemInit.java
+++ src/main/java/com/ambellic/ambellicportals/init/ItemInit.java
@@ -1,24 +1,23 @@
 package com.ambellic.ambellicportals.init;
 
 import com.ambellic.ambellicportals.AmbellicPortals;
-
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.registries.ObjectHolder;
 
-@Mod.EventBusSubscriber(modid = AmbellicPortals.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+
+@Mod.EventBusSubscriber(modid = AmbellicPortals.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 @ObjectHolder(AmbellicPortals.MOD_ID)
-public class ItemInit
+public class iteminit
 {
-    public static final Item test_item = null;
+    public static final Item moon_powder = null;
 
     @SubscribeEvent
     public static void registerItems(final RegistryEvent.Register<Item> event)
     {
-        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("test_item"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("moon_powder"));
     }
 }
